3e2aadc45ed07d3a177710efdf43c3f5e5a65db2
Meri <mgogi13@freeuni.edu.ge>
2022-01-04 13:30:58 +0400

    Fix linter errors

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import Calculator from './components/Calculator';
 class App extends React.Component {
   constructor(props) {
     super(props);
+    this.state = {}
   }
 
   render() {
@@ -14,7 +15,6 @@ class App extends React.Component {
     );
   }
 }
-
 
 ReactDOM.render(
   <App />,
