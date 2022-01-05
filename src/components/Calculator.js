72cc78b2e7a0503157265f07bc38fab354c42b8a
Meri Gogichashvili <mgogi13@freeuni.edu.ge>
2022-01-05 17:08:47 +0400

    Events (#3)
    * Add linter files and actions
    
    * Fix linter errors
    
    * Fix linter errors
    
    * Fix linter errors
    
    * Fix linter errors
    
    * Fix linter errors
    
    * Fix linter errors
    
    * Fix linter errors
    
    * Add UI to the page
    
    * Add margins
    
    * Fix linter errors
    
    * Fix linter errors
    
    * Fix linter errors
    
    * Fix linter errors
    
    * Fix sizes
    
    * Add logic to the page
    
    * Add events and display
    
    * Add design
    
    * Solve conflicts
    
    * Fix linter errors
    
    * Update gitignore
    
    * Fix linter errors
    
    * Update Readme file
    
    * Update README.md

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -1,34 +1,51 @@
 import React from 'react';
+import calculate from '../logic/calculate';
 
 class Calculator extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {};
+    this.state = {
+      total: null,
+      next: null,
+      operation: null,
+    };
+  }
+
+  handleClick = (e) => {
+    const result = calculate(this.state, e.target.innerText);
+    this.setState(result);
   }
 
   render() {
+    const { total, next, operation } = this.state;
     return (
       <div className="calculator-grid">
-        <div className="output">0</div>
-        <button type="button">AC</button>
-        <button type="button">+/-</button>
-        <button type="button">%</button>
-        <button className="orange" type="button">&divide;</button>
-        <button type="button">7</button>
-        <button type="button">8</button>
-        <button type="button">9</button>
-        <button className="orange" type="button">&times;</button>
-        <button type="button">4</button>
-        <button type="button">5</button>
-        <button type="button">6</button>
-        <button className="orange" type="button">-</button>
-        <button type="button">1</button>
-        <button type="button">2</button>
-        <button type="button">3</button>
-        <button className="orange" type="button">+</button>
-        <button className="span-two" type="button">0</button>
-        <button type="button">.</button>
-        <button className="orange" type="button">=</button>
+        <div className="output">
+          <div className="prevOperand">
+            {total}
+            {operation}
+          </div>
+          <div className="currOperand">{next}</div>
+        </div>
+        <button type="button" onClick={this.handleClick}>AC</button>
+        <button type="button" onClick={this.handleClick}>+/-</button>
+        <button type="button" onClick={this.handleClick}>%</button>
+        <button className="orange" type="button" onClick={this.handleClick}>÷</button>
+        <button type="button" onClick={this.handleClick}>7</button>
+        <button type="button" onClick={this.handleClick}>8</button>
+        <button type="button" onClick={this.handleClick}>9</button>
+        <button className="orange" type="button" onClick={this.handleClick}>x</button>
+        <button type="button" onClick={this.handleClick}>4</button>
+        <button type="button" onClick={this.handleClick}>5</button>
+        <button type="button" onClick={this.handleClick}>6</button>
+        <button className="orange" type="button" onClick={this.handleClick}>-</button>
+        <button type="button" onClick={this.handleClick}>1</button>
+        <button type="button" onClick={this.handleClick}>2</button>
+        <button type="button" onClick={this.handleClick}>3</button>
+        <button className="orange" type="button" onClick={this.handleClick}>+</button>
+        <button className="span-two" type="button" onClick={this.handleClick}>0</button>
+        <button type="button" onClick={this.handleClick}>.</button>
+        <button className="orange" type="button" onClick={this.handleClick}>=</button>
       </div>
     );
   }
